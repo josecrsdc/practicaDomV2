47f96d86bf0975c84cdefb5b617b441bfe53cd7e
Jose <josecarlosrodriguezsanchez89@gmail.com>
2020-10-30 19:01:35 +0100

    finalizada

--- script/main.js
+++ script/main.js
@@ -1,10 +1,6 @@
-
-// let cajaCarrito, carrito;
-//document.addEventListener("DOMContentLoaded", function(event) {             
-    // Elementos DOM
-    // let cajaCatalogo = document.getElementById("catalogo-contenido");                
-    let cajaCarrito = document.getElementById("carrito-contenido");      
-    // let precioCarrito = document.getElementById("carrito-precioTotal");         
+         
+                  
+    let cajaCarrito = document.getElementById("carrito-contenido");             
     let vaciarCarrito = document.getElementById("carrito-vaciar").onclick = function () {
         limpiarCarrito();
     }; 
@@ -88,8 +84,7 @@
                 unidades--;
                 document.getElementById(idCarrito).getElementsByTagName("p")[1].innerText = unidades;
                 let precio = articuloCatalogo.getElementsByClassName("articulo-precio");
-                // Cambiar por split por si cambia el precio
-                precio = precio[0].textContent.substring(7,14).replace(/\./g, "");
+                precio = parseInt(precio[0].textContent.split(" ")[1].replace(/\./g, ""));
                 let precioFormat = new Intl.NumberFormat("es-ES").format(precio * unidades);
                 document.getElementById(idCarrito).getElementsByTagName("p")[2].innerText = "Precio Total: " + precioFormat + " €";
             }else {
@@ -170,7 +165,6 @@
     }
 
 
-    // Borra html de carrito --------------------FALTA CALCULO
     function limpiarCarrito() {
         let articulos = cajaCarrito.getElementsByClassName("carrito-articulo");
         Array.from(articulos).forEach(articulo => {
@@ -178,147 +172,3 @@
         });
         calculaTotal();
     }
-
-
-
-//-------------------------------------------------
-//--------------------CARRITO----------------------
-//-------------------------------------------------
-
-    
-    // function mostrarCarrito() {
-    //         cajaCarrito.innerHTML = "";
-    //         precioTotal = 0;
-    //         carrito = catalogo.filter(coche => coche.enCarrito == true);
-    //         carrito.forEach(coche => {
-    //             //Calcula precio total
-    //             precioTotal += parseInt(coche.precio)*coche.cantidad;
-
-    //             // Crear articulo
-                //  articulo = document.createElement("article");
-                //  articulo.className = "carrito-articulo";
-                //  articulo.id = "carrito" + articulo.id;
-        
-    //             // Crear imagen del articulo
-    //             creaImagen(coche);
-        
-    //             // Crear titulo y añadir texto
-    //             creaTitulo(coche);
-        
-    //             // Crear precio y añadir texto
-    //             creaPrecio(coche);
-
-    //             // Crear cantidad en el carrito
-    //             creaCantidad(coche);
-                
-    //             // Crear suma conjunto por articulo
-    //             creaSumaPorArticulo(coche);
-
-    //             // Crear contenedor botones
-    //             contenedorBotones = document.createElement("div");
-    //             contenedorBotones.className = "contenedor-botones";
-
-    //             // Crear boton borrar
-    //             creaBtnBorrar(coche);
-
-    //             // Crear boton borrar-todos
-    //             creaBtnBorrarAll(coche);
-
-    //             // Insertar elementos al articulo
-    //             articulo.appendChild(articuloImg);
-    //             articulo.appendChild(articuloTitulo);
-    //             articulo.appendChild(articuloPrecio);
-    //             articulo.appendChild(articuloCantidad);
-    //             articulo.appendChild(articuloSuma);
-    //             articulo.appendChild(contenedorBotones);
-    //             contenedorBotones.appendChild(articuloBtnBorrar);
-    //             contenedorBotones.appendChild(articuloBtnBorrarAll);
-        
-    //             // Insertar articulo a caja catalogo
-    //             cajaCarrito.appendChild(articulo);
-    //         });
-    //         let precioFormat = new Intl.NumberFormat("es-ES").format(precioTotal);
-    //         precioCarrito.innerHTML = "Precio Total Carrito: " + precioFormat + " €";
-    //     }
-
-    //     vaciarCarrito.onclick = function () {
-    //         carrito.forEach(coche => {
-    //             coche.cantidad = 0;
-    //             coche.enCarrito = false;
-    //         });
-    //         mostrarCarrito();
-    //     }
-
-
-    //     function existeEnCarrito(coche) {
-    //         return coche.enCarrito;
-    //     }
-    
-    //     function creaImagen(coche) {
-    //         articuloImg = document.createElement("img");
-    //         articuloImg.className = "articulo-img";
-    //         articuloImg.src = "./img/" + coche.foto + ".jpg";
-    //     }
-    
-    //     function creaTitulo(coche) {
-    //         articuloTitulo = document.createElement("p");
-    //         articuloTitulo.className = "articulo-titulo";
-    //         tituloTexto = document.createTextNode(coche.modelo);
-    //         articuloTitulo.appendChild(tituloTexto);
-    //     }
-    
-    //     function creaPrecio(coche) {
-    //         articuloPrecio = document.createElement("p");
-    //         articuloPrecio.className = "articulo-precio";
-    //         let logoPrecio = document.createElement("img");
-    //         logoPrecio.className = "precio-logo";
-    //         logoPrecio.src = "./img/precio-logo.png";
-    //         let precioFormat = new Intl.NumberFormat("es-ES").format(coche.precio);
-    //         precioTexto = document.createTextNode(precioFormat + " €");
-    //         articuloPrecio.appendChild(logoPrecio);
-    //         articuloPrecio.appendChild(precioTexto);
-    //     }
-    
-    //     function creaCantidad(coche) {
-    //         articuloCantidad = document.createElement("p");
-    //         articuloCantidad.className = "articulo-cantidad";
-    //         cantidadTexto = document.createTextNode(" x " + coche.cantidad);
-    //         articuloCantidad.appendChild(cantidadTexto);
-    //     }
-
-    //     function creaSumaPorArticulo(coche) {
-    //         articuloSuma = document.createElement("p");
-    //         articuloSuma.className = "articulo-suma";
-    //         let logoSuma = document.createElement("img");
-    //         let precioFormat = new Intl.NumberFormat("es-ES").format(coche.cantidad * coche.precio);
-    //         sumaTexto = document.createTextNode("Precio Total: " + precioFormat + " €");
-    //         articuloSuma.appendChild(sumaTexto);
-    //     }
-    
-    //     function creaBtnBorrar(coche) {
-    //         articuloBtnBorrar = document.createElement("p");
-    //         articuloBtnBorrar.className = "boton-borrar";
-    //         articuloBtnBorrar.onclick = function borrarUnidad() {
-    //             if (coche.cantidad > 1) {
-    //                 coche.cantidad--;
-    //             }else {
-    //                 coche.enCarrito = false;
-    //             }
-    //             mostrarCarrito();
-    //         }
-    //         botonTexto = document.createTextNode("Borrar");
-    //         articuloBtnBorrar.appendChild(botonTexto);
-    //     }
-
-    //     function creaBtnBorrarAll(coche) {
-    //         articuloBtnBorrarAll = document.createElement("p");
-    //         articuloBtnBorrarAll.className = "boton-borrarAll";
-    //         articuloBtnBorrarAll.onclick = function borrarAll() {
-    //             coche.cantidad = 0;
-    //             coche.enCarrito = false;
-    //             mostrarCarrito();
-    //         }
-    //         botonTexto = document.createTextNode("Borrar Todo");
-    //         articuloBtnBorrarAll.appendChild(botonTexto);
-    //     }
-    
